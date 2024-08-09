1a919c42f4fc086eb80e0f7b559370be7f18cb7c
Eyre <1294023567@qq.com>
2024-08-09 11:45:15 +0800

    社区首页+社区登录+社区核心功能+Redis高性能存储+Kafka异步消息系统+ElasticSearch分布式搜索

---
+++ src/main/java/com/eyre/community/dao/elasticsearch/DiscussPostRepository.java
@@ -0,0 +1,10 @@
+package com.eyre.community.dao.elasticsearch;
+
+import com.eyre.community.entity.DiscussPost;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DiscussPostRepository extends ElasticsearchRepository<DiscussPost, Integer> {
+
+}
---
+++ src/main/java/com/eyre/community/util/HostHolder.java
@@ -0,0 +1,22 @@
+package com.eyre.community.util;
+
+import org.springframework.stereotype.Component;
+
+import com.eyre.community.entity.User;
+
+@Component
+public class HostHolder {
+    private ThreadLocal<User> users = new ThreadLocal<>();
+
+    public void setUser(User user) {
+        users.set(user);
+    }
+
+    public User getUser() {
+        return users.get();
+    }
+
+    public void clear() {
+        users.remove();
+    }
+}
