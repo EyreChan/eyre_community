5d7b8e001e187454a329b4d93262ea46bd831d2c
Eyre <1294023567@qq.com>
2024-08-09 11:43:35 +0800

    社区首页+社区登录+社区核心功能+Redis高性能存储+Kafka异步消息系统+ElasticSearch分布式搜索

--- src/main/java/com/eyre/community/controller/HomeController.java
+++ src/main/java/com/eyre/community/controller/HomeController.java
@@ -4,7 +4,9 @@ import com.eyre.community.entity.DiscussPost;
 import com.eyre.community.entity.Page;
 import com.eyre.community.entity.User;
 import com.eyre.community.service.DiscussPostService;
+import com.eyre.community.service.LikeService;
 import com.eyre.community.service.UserService;
+import com.eyre.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,12 +19,15 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-public class HomeController {
+public class HomeController implements CommunityConstant {
     @Autowired
     private DiscussPostService discussPostService;
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private LikeService likeService;
 
     @RequestMapping(path = "/index", method = RequestMethod.GET)
     public String getIndexPage(Model model, Page page) {
@@ -39,10 +44,19 @@ public class HomeController {
                 map.put("post", post);
                 User user = userService.findUserById(post.getUserId());
                 map.put("user", user);
+
+                long likeCount = likeService.findEntityLikeCount(ENTITY_TYPE_POST, post.getId());
+                map.put("likeCount", likeCount);
+
                 discussPosts.add(map);
             }
         }
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
+    }
+
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
     }
 }
